f948482d050f96edec6423e5616b988610f6f6e9
ranjeet2003 <ranjeetgautam13032@gmail.com>
2021-01-26 12:31:36 +0530

    working event loop

--- HOW-NODE-WORKS/event-loop.js
+++ HOW-NODE-WORKS/event-loop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+
 const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 2;
 
 setTimeout(() => console.log('Timer 1 is finished'), 0);
 setImmediate(() => console.log('Immediate 1 is finished'));
@@ -14,7 +16,16 @@ fs.readFile('test-file.txt', () => {
   setImmediate(() => console.log('Immediate 1 is finished'));
 
   process.nextTick(() => console.log('Process.nextTick()'));
-  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512', () => {
     console.log(Date.now() - start, 'Password encrypted');
   });
 });
